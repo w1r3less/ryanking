3c4a3d45344d522f5e2755a5e1e1da13cc60355a
RyanKingWorkrise <ryan.king@workrise.com>
2024-12-21 22:51:48 +0000

    Add analytics tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata: Metadata = {
   title: 'v0 App',
@@ -13,7 +14,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
+      
     </html>
   )
 }
